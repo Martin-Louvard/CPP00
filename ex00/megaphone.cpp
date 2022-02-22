1701a51504c28f986ba6b3b0487fa483d7913351
Martin LOUVARD <malouvar@e2r4p8.clusters.42paris.fr>
2022-02-22 15:28:24 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int i = 1;
+
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        while (argv[i])
+        {
+            
+        }
+
+    }
+    return 0;
+}
